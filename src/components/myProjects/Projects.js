67580e6f6eac553eb83571f83cef56c7bdf0e001
Kostya Kokhanov <knuckostya@gmail.com>
2023-03-05 20:45:16 +0200

    02.03.2023

--- src/components/myProjects/Projects.js
+++ src/components/myProjects/Projects.js
@@ -5,6 +5,7 @@ import Project from "./Project/Project";
 import {Title} from "../../common/title/Title";
 import todoImage from '../../common/icons/todo-list.jpg'
 import socialNetworkImage from '../../common/icons/social-network.jpg'
+import counter from '../../common/icons/counter.JPG'
 
 const Projects = (props) => {
     return (
@@ -27,6 +28,10 @@ const Projects = (props) => {
                         link={'https://www.google.com/?&bih=829&biw=1730&hl=ru'}
                         title={'Project2'} description={'Miami'}
                         img={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwYvRUF6CwX1jDAEF_la46hU7iCOboOcolva6DSikp2YUXnkpBfZIkYmyzsRe5uO7Zw-Q&usqp=CAU'}/>
+                    <Project
+                        link={'https://www.google.com/?&bih=829&biw=1730&hl=ru'}
+                        title={'Counter'} description={'Stack:Redux,Thunk,Unit-Test,Local Storage'}
+                        img={counter}/>
                 </div>
             </div>
         </div>
